348be00d09ad18d6fa1238080d442edebbb5a385
prakash-9610 <prakashchoyal02@gmail.com>
2025-10-17 15:29:19 +0530

    datatype summary

--- 01-basics/03datatypesconversion.js
+++ 01-basics/03datatypesconversion.js
@@ -9,7 +9,25 @@
 // console.log(typeof valueInNumber);
 // "33" =>33
 //"33abc" =>NaN
-// TRUE=1 
-let someNumber = 33;
-let stringNumber = String(someNumber)
-console.log(stringNumber);
+// TRUE=1
+// let someNumber = 33;
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// let str1 = "prakash"
+// let str2 = "choyal"
+// let str3 = str1 +" "+ str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let gamCounter = 100;
+gamCounter++;
+console.log(gamCounter);
---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// primitive data tyepes
+//7 types: String, Number, Boolean, Null, Undefined,Symbol,BigInt
+
+
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail;
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+// console.log(id === anotherId);
+
+
+
+
+//Reference(Non Premitive)
+// Array ,Object, Function
+const bigNumber = 56452484634626149n;
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+    name: "prakash",
+    age: 22
+};
+const myfunction = function () {
+    console.log("hello world");
+}
